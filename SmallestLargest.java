14b6445fc468ae132618e8bd1c0c7fb2fd04bd76
Aasha Shakya <178453028+Ash-1412@users.noreply.github.com>
2024-08-27 19:10:51 +0000

    initial commit

--- SmallestLargest.java
+++ SmallestLargest.java
@@ -1,12 +1,41 @@
-//put your name here
+//Aasha Shakya
 public class SmallestLargest{
-    public static void main(String[] args){
 
+    public static int[] arr = new int[10];
+
+    public static void main(String[] args){
+        for(int i = 0; i < 10; i++)
+        {
+            arr[i] = (int)((Math.random()*100)+1);
+            System.out.println(arr[i]);
+
+        }
+        System.out.println("");
+        System.out.println(smallestValue());
+        System.out.println(largestValue());
     }
     public static int smallestValue(){
 
+        int small = arr[0];
+        for(int i = 0; i < 9; i++)
+        {
+            if(arr[i] < small)
+            {
+                small = arr[i];
+            }
+        }
+        return small;
     }
     public static int largestValue(){
+        int large = arr[0];
+        for(int i = 0; i < 9; i++)
+        {
+            if(arr[i] > large)
+            {
+                large = arr[i];
+            }
+        }
+        return large;
         
     }
 }
